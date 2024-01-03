1b5cc0311ef72c86f5f92b6d09c6e25fc7f85850
nuruposan <stkinosh@gmail.com>
2024-01-03 13:07:17 +0900

    add drawDialog function

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,15 @@
 #include <SD.h>
 
 #define SD_ACCESS_SPEED 15000000
-#define COLOR16(r,g,b) (int16_t)((r>>3)<<11)|((g>>2)<<5)|(b>>3)
+#define COLOR16(r, g, b) (int16_t)((r >> 3) << 11) | ((g >> 2) << 5) | (b >> 3)
 
-// BluetoothSerial gpsSerial;
+BluetoothSerial gpsSerial;
 
 const char APP_NAME[] = "SmallStepM5S";
 const char APP_VERSION[] = "v0.01";
 
 const int16_t COLOR_SCREEN = BLACK;
-const int16_t COLOR_TITLE_BACK = COLOR16(0, 128, 255);
+const int16_t COLOR_TITLE_BACK = COLOR16(128, 192, 255);
 const int16_t COLOR_TITLE_TEXT = BLACK;
 const int16_t COLOR_ICON_BODY = BLACK;
 const int16_t COLOR_ICON_TEXT = LIGHTGREY;
@@ -24,15 +24,7 @@ const int16_t COLOR_MENU_SEL_BACK = LIGHTGREY;
 const int16_t COLOR_NAVI_BACK = LIGHTGREY;
 const int16_t COLOR_NAVI_TEXT = BLACK;
 
-typedef enum _appstatus {
-  APP_IN_MENU  = 0x000000,
-  APP_DO_MENU1 = 0x001000,
-  APP_DO_MENU2 = 0x002000,
-  APP_DO_MENU3 = 0x003000,
-  APP_DO_MENU4 = 0x004000,
-  APP_DO_MENU5 = 0x005000,
-  APP_DO_MENU6 = 0x006000
-} appstatus_t;
+typedef enum _buttonid { BI_BTN_A = 0, BI_BTN_B = 1, BI_BTN_C = 2 } buttonid_t;
 
 typedef struct _naviitem {
   const char *caption;
@@ -53,7 +45,18 @@ typedef struct _mainmenu {
   int8_t selectedIndex;
 } mainmenu_t;
 
-//appstatus_t status;
+typedef struct _loggerinfo {
+  int8_t found;
+  char remoteName[20];
+  uint8_t address[6];
+} loggerinfo_t;
+
+typedef struct _appconfig {
+  loggerinfo_t logger;
+} appconfig_t;
+
+appconfig_t status;
+loggerinfo_t loggerDiscovered;
 
 TFT_eSprite sprite = TFT_eSprite(&M5.Lcd);
 naviitem_t naviButtons[3];
@@ -63,63 +66,164 @@ mainmenu_t mainMenu;
  * M5.BtnA.wasPressedの戻り値をそのまま返す。
  * インスタンスのメンバ関数を関数ポインタから呼ぶためのラッパー関数
  */
-uint8_t M5BtnA_wasPressed() {
-  return (M5.BtnA.wasPressed());
-}
+uint8_t M5BtnA_wasPressed() { return (M5.BtnA.wasPressed()); }
 
 /**
  * M5.BtnB.wasPressedの戻り値をそのまま返す。
  * インスタンスのメンバ関数を関数ポインタから呼ぶためのラッパー関数
  */
-uint8_t M5BtnB_wasPressed() {
-  return (M5.BtnB.wasPressed());
-}
+uint8_t M5BtnB_wasPressed() { return (M5.BtnB.wasPressed()); }
 
 /**
  * M5.BtnC.wasPressedの戻り値をそのまま返す。
  * インスタンスのメンバ関数を関数ポインタから呼ぶためのラッパー関数
  */
-uint8_t M5BtnC_wasPressed() {
-  return (M5.BtnC.wasPressed());
+uint8_t M5BtnC_wasPressed() { return (M5.BtnC.wasPressed()); }
+
+/**
+ * NMEAコマンドのチェックサム計算を行う (printfのフォーマット%Xで送る値を生成)
+ */
+byte calcNmeaChecksum(char *cmd, byte len) {
+  byte chk = 0;
+  for (byte i = 0; (i < len) && (cmd[i] != 0); i++) {
+    chk ^= (byte)(cmd[i]);
+  }
+  return chk;
 }
 
 /**
- * 
+ * NMEAコマンドにチェックサムをつけてSerialBTに書き出す
+ */
+bool sendNmeaCommand(char *cmd, byte len) {
+  if (gpsSerial.connected() == false) return false;
+
+  word len2 = (word)(len) + 8;
+  char buf[len2];
+
+  byte chk = calcNmeaChecksum(cmd, len);
+  memset(buf, 0, len2);
+  sprintf(buf, "$%s*%X\r\n", cmd, chk);
+
+  Serial.print("[DEBUG] sendNmeaCommand: >");
+
+  for (short i = 0; (i < len2) && (buf[i] != 0); i++) {
+    gpsSerial.write(buf[i]);
+    Serial.print(buf[i]);
+  }
+
+  return true;
+}
+
+// PMTK182,7コマンドを送る (ログのダウンロードコマンド)
+void sendDownloadCommand(int startPos, int reqSize) {
+  char cmdstr[32];
+  sprintf(cmdstr, "PMTK182,7,%08X,%08X", startPos, reqSize);
+  sendNmeaCommand(cmdstr, sizeof(cmdstr));
+}
+
+void onBluetoothDiscoverCallback(esp_spp_cb_event_t event,
+                                 esp_spp_cb_param_t *param) {
+  if (event == ESP_SPP_OPEN_EVT) {
+    loggerDiscovered.found = true;
+    memcpy(&loggerDiscovered.address, &param->open.rem_bda,
+           sizeof(loggerDiscovered.address));
+  }
+}
+
+void drawDialog(const char *title, const char *msg) {
+  const int16_t MARGIN = 4;
+  const int16_t DIALOG_X = MARGIN;
+  const int16_t DIALOG_Y = 24 + MARGIN;
+  const int16_t DIALOG_W = M5.Lcd.width() - (MARGIN * 2);
+  const int16_t DIALOG_H = (M5.Lcd.height() - 48) - (MARGIN * 2);
+  const int16_t TITLE_X = 2;
+  const int16_t TITLE_Y = 2;
+  const int16_t MSG_X = 8;
+  const int16_t MSG_Y = 28;
+
+  sprite.setColorDepth(8);
+  sprite.createSprite(DIALOG_W, DIALOG_H);
+
+  sprite.fillRect(0, 0, sprite.width(), sprite.height(), LIGHTGREY);
+  sprite.fillRect(0, 0, sprite.width(), 24, COLOR16(0, 128, 255));
+  sprite.drawRect(0, 0, sprite.width(), sprite.height(), BLUE);
+
+  sprite.setTextSize(1);
+  sprite.setTextColor(BLACK);
+  sprite.drawString(title, TITLE_X, TITLE_Y, 4);
+  sprite.drawString(msg, MSG_X, MSG_Y, 4);
+
+  sprite.pushSprite(DIALOG_X, DIALOG_Y);
+  sprite.deleteSprite();
+}
+
+/**
+ *
  */
 void onDownloadLogMenuSelected() {
-  Serial.printf("[DEBUG] onDownloadLogMenuSelected\n");
+  const char gpsPinCode[] = "0000";  // SPP接続時のPinコード。通常は"0000"で固定
+
+  gpsSerial.register_callback(onBluetoothDiscoverCallback);
+  gpsSerial.setTimeout(5000);
+  gpsSerial.setPin(gpsPinCode);  // Pinコードをセット。これがないと繋がらない
+
+  // char deviceNames[][] = {"747PRO GPS", "Holux_M-241"};
+
+  drawDialog("Discovering", "Try to connect to 747PRO GPS...");
+  if (gpsSerial.connect("747PRO GPS")) {
+    drawDialog("Success", "logger found!");
+
+    //    for (int i = 0; i < 6; i++) {
+    //      Serial.printf("%02x", loggerDiscovered.address[i]);
+    //    }
+    //    Serial.printf("\n");
+
+    char cmd[] = "PMTK605";
+    sendNmeaCommand(cmd, sizeof(cmd));
+
+    int n = 0;
+    sprite.setCursor(0, 20);
+    while (n < 500) {
+      while (gpsSerial.available()) {
+        Serial.print((char)gpsSerial.read());
+        n++;
+      }
+      delay(1);
+    }
+    gpsSerial.disconnect();
+  } else {
+    drawDialog("Failed", "No logger found!");
+  }
 }
 
 /**
- * 
+ *
  */
-void onFixRTCMenuSelected() {
-  Serial.printf("[DEBUG] onFixRTCMenuSelected\n");
-}
+void onFixRTCMenuSelected() { Serial.printf("[DEBUG] onFixRTCMenuSelected\n"); }
 
 /**
- * 
+ *
  */
 void onSetPresetConfigMenuSelected() {
   Serial.printf("[DEBUG] onSetPresetConfigMenuSelected\n");
 }
 
 /**
- * 
+ *
  */
 void onShowLocationMenuSelected() {
   Serial.printf("[DEBUG] onShowLocationMenuSelected\n");
 }
 
 /**
- * 
+ *
  */
 void onEraseLogMenuSelected() {
   Serial.printf("[DEBUG] onEraseLogMenuSelected\n");
 }
 
 /**
- * 
+ *
  */
 void onAppSettingsMenuSelected() {
   Serial.printf("[DEBUG] onAppSettingsMenuSelected\n");
@@ -128,59 +232,70 @@ void onAppSettingsMenuSelected() {
 /**
  * SDカードの状態表示アイコンの描画
  */
-void drawSDcardIcon(TFT_eSprite *spr, const int16_t POS_X, const int16_t POS_Y) {
+void drawSDcardIcon(TFT_eSprite *spr, const int16_t POS_X,
+                    const int16_t POS_Y) {
   const int16_t ICON_W = 17;
   const int16_t ICON_H = 20;
   const int16_t PIN_W = 2;
   const int16_t PIN_H = 4;
 
   // SDカードの外枠を描画
-  spr->fillRoundRect((POS_X + 2), POS_Y, (ICON_W - 2), 8, 2, COLOR_ICON_BODY);  // 本体
-  spr->fillRoundRect(POS_X, (POS_Y + 6), ICON_W, (ICON_H - 6), 2, COLOR_ICON_BODY);  // 本体
-  for (int i = 0; i < 4; i++) {                                  // 端子部
-    spr->fillRect((POS_X + 4) + ((PIN_W + 1) * i), (POS_Y + 1), PIN_W, PIN_H, COLOR_ICON_SD_PIN);
+  spr->fillRoundRect((POS_X + 2), POS_Y, (ICON_W - 2), 8, 2,
+                     COLOR_ICON_BODY);  // 本体
+  spr->fillRoundRect(POS_X, (POS_Y + 6), ICON_W, (ICON_H - 6), 2,
+                     COLOR_ICON_BODY);  // 本体
+  for (int i = 0; i < 4; i++) {         // 端子部
+    spr->fillRect((POS_X + 4) + ((PIN_W + 1) * i), (POS_Y + 1), PIN_W, PIN_H,
+                  COLOR_ICON_SD_PIN);
   }
 
   File root = SD.open("/");
-  if (root) {  // SDカードが使用可能な場合 (ルートディレクトリがオープンできる)
+  if (root) {  // SDカードが使用可能な場合
+               // (ルートディレクトリがオープンできる)
     // テスト用に開いたファイルを閉じる
     root.close();
 
-    { // 状態表示
+    {  // 状態表示
       spr->setTextSize(1);
       spr->setTextColor(COLOR_ICON_TEXT);
       spr->drawCentreString("SD", (POS_X + 9), (POS_Y + 9), 1);
     }
   } else {  // SDカードが使用不能な場合
     spr->drawEllipse((POS_X + 8), (POS_Y + 12), 5, 5, COLOR_ICON_ERROR);
-    spr->drawLine((POS_X + 11), (POS_Y + 9), (POS_X + 5), (POS_Y + 15), COLOR_ICON_ERROR);
+    spr->drawLine((POS_X + 11), (POS_Y + 9), (POS_X + 5), (POS_Y + 15),
+                  COLOR_ICON_ERROR);
   }
 }
 
 /**
  * バッテリーアイコンの描画
  */
-void drawBatteryIcon(TFT_eSprite *spr, const int16_t POS_X, const int16_t POS_Y) {
+void drawBatteryIcon(TFT_eSprite *spr, const int16_t POS_X,
+                     const int16_t POS_Y) {
   const int16_t ICON_W = 21;
   const int16_t ICON_H = 20;
   const int16_t METER_W = 3;
   const int16_t METER_H = 12;
-  
+
   // バッテリーの外枠を描画
-  spr->fillRoundRect(POS_X, POS_Y, (ICON_W - 2), ICON_H, 2, COLOR_ICON_BODY);  // 本体
-  spr->fillRect((POS_X + (ICON_W - 2)), (POS_Y + 6), 2, 8, COLOR_ICON_BODY);  // 凸部
+  spr->fillRoundRect(POS_X, POS_Y, (ICON_W - 2), ICON_H, 2,
+                     COLOR_ICON_BODY);  // 本体
+  spr->fillRect((POS_X + (ICON_W - 2)), (POS_Y + 6), 2, 8,
+                COLOR_ICON_BODY);  // 凸部
 
   if (M5.Power.canControl()) {
-    // 4段階でバッテリーレベルを取得 15%以下、40%以下、65%以下、100%以下
+    // 4段階でバッテリーレベルを取得 0-bias%以下、2540%以下、65%以下、100%以下
     int8_t battBias = 15;
     int8_t battLevel = (battBias + M5.Power.getBatteryLevel()) / 25;
 
     // バッテリーレベルの表示
     for (int i = 0; i < battLevel; i++) {
-      spr->fillRect((POS_X + 2) + ((METER_W+1) * i), (POS_Y + 4), METER_W, METER_H, COLOR_ICON_TEXT);
+      spr->fillRect((POS_X + 2) + ((METER_W + 1) * i), (POS_Y + 4), METER_W,
+                    METER_H, COLOR_ICON_TEXT);
     }
     if (battLevel == 0) {
-      spr->fillRect((POS_X + 2), (POS_Y + 4), METER_W, METER_H, COLOR_ICON_TEXT);
+      spr->fillRect((POS_X + 2), (POS_Y + 4), METER_W, METER_H,
+                    COLOR_ICON_TEXT);
     }
   } else {  // バッテリーレベル不明の表示
     spr->setTextSize(2);
@@ -203,7 +318,7 @@ void drawTitleBar() {
   const int16_t BATTERY_ICON_Y = 2;
   const int16_t SDCARD_ICON_X = (BATTERY_ICON_X - 20);
   const int16_t SDCARD_ICON_Y = 2;
-  
+
   // スプライト領域の初期化
   sprite.setColorDepth(8);
   sprite.createSprite(TITLEBAR_W, TITLEBAR_H);
@@ -217,8 +332,10 @@ void drawTitleBar() {
   }
 
   // 状態表示アイコンの表示
-  drawSDcardIcon(&sprite, SDCARD_ICON_X, SDCARD_ICON_Y); // SDカードステータス
-  drawBatteryIcon(&sprite, BATTERY_ICON_X, BATTERY_ICON_Y); // バッテリーステータス
+  drawSDcardIcon(&sprite, SDCARD_ICON_X,
+                 SDCARD_ICON_Y);  // SDカードステータス
+  drawBatteryIcon(&sprite, BATTERY_ICON_X,
+                  BATTERY_ICON_Y);  // バッテリーステータス
 
   // 描画した内容を画面に反映し、スプライト領域を開放
   sprite.pushSprite(TITLEBAR_X, TITLEBAR_Y);
@@ -230,9 +347,9 @@ void drawMainMenu() {
   const int16_t MENUAREA_H = M5.Lcd.height() - (24 + 24);
   const int16_t MENUAREA_X = 0;
   const int16_t MENUAREA_Y = 24;
-  const int16_t MENUBTN_W = 98; // ボタンのサイズ指定
+  const int16_t MENUBTN_W = 98;  // ボタンのサイズ指定
   const int16_t MENUBTN_H = 83;
-  const int16_t MENUBTN1_X = 5; // 配置の基準とする左上ボタンの位置
+  const int16_t MENUBTN1_X = 5;  // 配置の基準とする左上ボタンの位置
   const int16_t MENUBTN1_Y = 9;
   const int16_t MARGIN_X = 8;
   const int16_t MARGIN_Y = 7;
@@ -257,14 +374,12 @@ void drawMainMenu() {
                            COLOR_MENU_BACK);
     }
 
-    // ボタンのアイコンを描画
-    //M5.Lcd.drawBitmap(ICON_X, ICON_Y, 60, 60, iconData);
-
-    { // ボタンのキャプションを描画
+    {  // ボタンのキャプションを描画
       sprite.setTextSize(1);
       sprite.setTextColor(COLOR_MENU_TEXT);
-      sprite.drawCentreString(mainMenu.items[i].caption, POS_X+(MENUBTN_W/2),
-                              POS_Y+(MENUBTN_H - 12), 1);
+      sprite.drawCentreString(mainMenu.items[i].caption,
+                              POS_X + (MENUBTN_W / 2), POS_Y + (MENUBTN_H - 12),
+                              1);
     }
   }
 
@@ -275,7 +390,8 @@ void drawMainMenu() {
 
 /**
  * 物理ボタンの操作ナビゲーションボタンを描画する。
- * ボタンのキャプションや有効状態はグローバル変数のnaviitem_t naviButtons[3]を参照
+ * ボタンのキャプションや有効状態はグローバル変数のnaviitem_t
+ * naviButtons[3]を参照
  */
 void drawNaviBar() {
   const int16_t NAVIBAR_W = M5.Lcd.width();
@@ -303,7 +419,7 @@ void drawNaviBar() {
 
     // ボタンの枠
     sprite.fillRoundRect(POS_X, POS_Y, NAVIBTN_W, NAVIBTN_H, 2,
-                          COLOR_NAVI_BACK);
+                         COLOR_NAVI_BACK);
 
     // キャプション
     sprite.setTextSize(1);
@@ -344,11 +460,14 @@ void setup() {
   M5.Lcd.setBrightness(32);
   M5.Lcd.clearDisplay(BLACK);
 
-    // スプライトの初期化
+  // スプライトの初期化
   sprite.setColorDepth(8);
 
   // SDカードの開始
   SD.begin(GPIO_NUM_4, SPI, SD_ACCESS_SPEED);
+
+  // BT初期化
+  gpsSerial.begin(APP_NAME, true);  // start bluetooth serial as a master
 
   memset(&naviButtons, 0, sizeof(naviButtons));
   naviButtons[0].caption = "Prev";
@@ -382,16 +501,16 @@ void setup() {
 }
 
 void loop() {
-  M5.update(); // M5Stackのステータス更新
+  M5.update();  // M5Stackのステータス更新
 
   // 物理ボタンの処理 (ナビボタンを押したものとして扱う)
-  for (int i=0; i<3; i++) {
-    naviitem_t *nb = &naviButtons[i]; // 判定対象のボタンを取得
-    
+  for (int i = 0; i < 3; i++) {
+    naviitem_t *nb = &naviButtons[i];  // 判定対象のボタンを取得
+
     // 物理ボタンが押されていればイベントハンドラを呼び出す
     if ((nb->disabled == false) && (nb->wasPressed())) {
-      if (nb->onClick != NULL) nb->onClick(); 
-      break;  // 同時押しがあっても両方処理しない
+      if (nb->onClick != NULL) nb->onClick();
+      break;  // 左・中・右の順に処理。同時に複数の処理はしない
     }
   }
 
