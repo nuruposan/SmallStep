ceb3f224dcf810d711a855daa53d5594eeafb75e
nuruposan <8068623+nuruposan@users.noreply.github.com>
2024-03-04 22:48:23 +0900

    ロガー周りのクラス化、アイコン追加

--- include/LoggerManager.hpp
+++ include/LoggerManager.hpp
@@ -1,42 +1,41 @@
 #pragma once
 
 #include <BluetoothSerial.h>
+#include <SD.h>
 
-typedef enum _flashsize {
+#include "ReceiveBuffer.hpp"
+
+typedef enum _sizeinfo {
+  SIZE_REPLY = 0x00000800,
   SIZE_8MBIT = 0x00100000,
   SIZE_16MBIT = 0x00200000,
-  SIZE_32MBIT = 0x00400000,
-  SIZE_DEFAULT = 0x00400000
-} flashsize_t;
-
-typedef enum _recordmethod {
-  RECORD_OVERWRAP = 1,
-  RECORD_FULLSTOP = 2
-} recordmethod_t;
-
-typedef enum _resultcode { RESULT_FAILED = 2, RESULT_SUCCESS = 3 } resultcode_t;
+  SIZE_32MBIT = 0x00400000
+} sizeinfo_t;
 
 class LoggerManager {
  private:
-  static const long SIZE_LOG_REPLY = 0x00000800;  // 2 Kbyte (FIXED)
-
-  // ReceiveBuffer *buffer;
-  bool connected;
-  char pinCode[8];
+  bool connectedFlag;
   char address[6];
   BluetoothSerial *gpsSerial;
+  ReceiveBuffer *buffer;
 
-  unsigned char calcChecksum(char *cmd, unsigned char len);
-  bool sendCommand(char *cmd, unsigned char len);
+  uint8_t calcNmeaChecksum(const char *cmd, uint8_t len);
+  bool sendNmeaCommand(const char *cmd, uint16_t len);
+  static int32_t modelIdToFlashSize(uint16_t modelId);
 
  public:
   LoggerManager();
   ~LoggerManager();
-  bool connect(char *address);
+
+  bool connect(uint8_t *address);
+  bool connected();
   void disconnect();
-  bool isConnected();
-  bool serialAvailable();
-  //    bool isReplyReady();
-  bool extractColumn(unsigned char column, char *buf, unsigned int len);
-  unsigned char readSerial();
+  bool sendDownloadCommand(int startPos, int reqSize);
+  bool getLogEndAddress(int32_t *size);
+  bool downloadFlashData(File *binFile, int32_t endAddr, void (*progress)(int));
+  // int32_t clearFlash();
+  // int32_t setLogMode(bool overrite);
+  // int32_t setLogByDistance(int16_t distance);
+  // int32_t setLogBySpeed(int16_t speed);
+  // int32_t setLogByTime(int16_t time);
 };
--- include/ReceiveBuffer.hpp
+++ include/ReceiveBuffer.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <Arduino.h>
-#define BP_BUFFER_SIZE 160
+#define BP_BUFFER_SIZE 512
 
 typedef struct bufferpage {
   char buf[BP_BUFFER_SIZE + 1];
@@ -12,18 +12,18 @@ class ReceiveBuffer {
  private:
   bufferpage_t *rootPage;
   bufferpage_t *currentPage;
-  uint8_t ptr;
+  uint16_t ptr;
   uint32_t dataLength;
   uint8_t columnCount;
-  uint8_t calculatedChecksum;
+  uint8_t expectedChecksum;
   uint8_t receivedChecksum;
 
-  static bool isTextChar(const char ch);
-  static uint8_t hexCharToByte(const char ch);
+  static bool isTextChar(char ch);
+  static uint8_t hexCharToByte(char ch);
   bufferpage_t *allocatePage(bufferpage_t **pg);
   void freePages(bufferpage_t *tgp);
-  void appendToBuffer(const char ch);
-  void updateChecksum(const char ch);
+  void appendToBuffer(char ch);
+  void updateChecksum(char ch);
 
  public:
   ReceiveBuffer();
@@ -33,8 +33,7 @@ class ReceiveBuffer {
   char get();
   bool readHexByteFull(byte *by);
   bool readHexByteHalf(byte *by);
-  bool readColumnAsInt(const uint8_t clm, int32_t *num);
-  bool seekToColumn(const uint8_t clm);
-  bool isChecksumCorrect();
+  bool readColumnAsInt(uint8_t clm, int32_t *num);
+  bool seekToColumn(uint8_t clm);
   bool match(const char *str);
 };
